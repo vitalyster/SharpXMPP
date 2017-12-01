4adb850c355d4a8cd63776971a5eae0c1bdeac29
Roman Rudyak <roma.rudyak@gmail.com>
2017-12-01 17:05:45 +0200

    Add empty Xamarin project struncture for test

---
+++ Xamarin/SharpXMPP.Client.Droid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace SharpXMPP.Client.Droid
+{
+    [Activity(Label = "SharpXMPP.Client.Droid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
