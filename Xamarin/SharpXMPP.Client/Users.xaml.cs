61cb5bf9dbe3f1f77c3b8e663d5f68ac69dd8cb6
Roman Rudyak <roma.rudyak@gmail.com>
2017-12-01 19:26:04 +0200

    Add use of SharpXMPP in Xamarin Client
    For test can be used https://chat.chinwag.im

--- Xamarin/SharpXMPP.Client/Users.xaml.cs
+++ Xamarin/SharpXMPP.Client/Users.xaml.cs
@@ -15,11 +15,22 @@ namespace SharpXMPP.Client
 	{
         private readonly XmppClient _xmppconnetction;
 
-        public Users()
+        public Users(XmppClient connection)
         {
             InitializeComponent();
-
+            _xmppconnetction = connection;
+            _users.ItemsSource = connection.RosterManager.Roster;
         }
 
+        public void RosterSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            var rester = (RosterItem)e.SelectedItem;
+            _users.SelectedItem = null;
+            Navigation.PushModalAsync(new Chat(rester, _xmppconnetction));
+        }
     }
 }
