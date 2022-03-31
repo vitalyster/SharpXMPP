d40c295a1210d28ab5a1e71ec2340638977768fd
Vitaly Takmazov <vitalyster@gmail.com>
2022-03-31 18:12:52 +0300

    Project cleanup
    * cleanup compiler warning
    * untrack autogenerated file

--- SharpXMPP.Xamarin/SharpXMPP.Client.iOS/Main.cs
+++ SharpXMPP.Xamarin/SharpXMPP.Client.iOS/Main.cs
@@ -1,4 +1,4 @@
-﻿using UIKit;
+using UIKit;
 
 namespace SharpXMPP.Client.iOS
 {
@@ -9,7 +9,7 @@ namespace SharpXMPP.Client.iOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
         }
     }
 }
