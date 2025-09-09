65f1275f4efead5e13a5e70ca032fc4798ad33c7
Vitaly Takmazov <vitalyster@gmail.com>
2025-09-09 07:41:11 +0300

    Xamarin -> MAUI migration

---
+++ SharpXMPP.Mobile/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SharpXMPP.Client")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SharpXMPP.Client.Pages")]
